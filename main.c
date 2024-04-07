f4efcbff29d08ef4fd899477de39f334838a0cae
Balas Vlad-George <vlad_george.balas@stud.acs.upb.ro>
2024-04-07 17:12:53 +0300

    Generare fisiere goale

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
-    printf("Hello World");
+int main(int argc, char *argv[]){
+    //extrgere fisiere
+    FILE* file_cerinte = fopen(*(argv + 1), "r");
+    FILE* file_date = fopen(*(argv + 2), "r");
+    FILE* file_iesire = fopen(*(argv + 3), "w");
+
+    fprintf(file_iesire, "Hello world");
+
+    fclose(file_cerinte);
+    fclose(file_date);
+    fclose(file_iesire);
+
     return 0;
 }
