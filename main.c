d9d5a77f60813fcae0f442d72c1685a9dbf99530
Balas Vlad-George <vlad_george.balas@stud.acs.upb.ro>
2024-05-09 23:00:23 +0300

    adaugare task 1

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "./libs/data/data.h"
+#include "./libs/list/list.h"
+#include "./libs/list/utils.h"
+
+extern struct list_node* task1(FILE* _fff_dta);
 
 int main(int argc, char *argv[]){
-    
+    FILE* fff_tsk = fopen(*(argv + 1), "r");
+    FILE* fff_dta = fopen(*(argv + 2), "r");
+    FILE* fff_out = fopen(*(argv + 3), "w");
 
-
-    /*//extrgere fisiere
-    FILE* file_cerinte = fopen(*(argv + 1), "r");
-    FILE* file_date = fopen(*(argv + 2), "r");
-    FILE* file_iesire = fopen(*(argv + 3), "w");
-
-    fprintf(file_iesire, "Hello world");
-
-    fclose(file_cerinte);
-    fclose(file_date);
-    fclose(file_iesire);
-    */
+    struct list_node* tsk_1_lst = task1(fff_dta);
 }
