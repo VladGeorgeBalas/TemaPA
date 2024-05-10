8e2c6d87f13e10044a88cf7c47a7de96aab5f5d5
Balas Vlad-George <vlad_george.balas@stud.acs.upb.ro>
2024-05-10 14:25:15 +0300

    inceput task 2

--- comps/task2/task2.c
+++ comps/task2/task2.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../libs/data/data.h"
+#include "../../libs/list/list.h"
+#include "../../libs/list/utils.h"
 
+void task2(struct list_node* _hed_lst){
+    //aflare numar de echipe
+    struct list_node* tmp = _hed_lst->next;
+    unsigned int num_tem = 0;
+
+    while(tmp)
+    {
+        num_tem++;
+        tmp = tmp->next;
+    }
+
+    fprintf(stderr, "numarul de echipe : ", num_tem);
+
+    //gasirea numarului maxim de echipe
+    unsigned int max_num_tem = 1;
+
+    while(num_tem!=1)
+        num_tem/=2, max_num_tem*=2;
+
+    fprintf(stderr, "maximul de echipe ramase : %d", max_num_tem);
+
+    //parcurgerea listei plus eliminare
+}
