4100f51634b84a4644f47a7a25dc81a6d5f871b9
Balas Vlad-George <vlad_george.balas@stud.acs.upb.ro>
2024-05-11 17:33:19 +0300

    reparatii task 1

--- comps/task1/task1.c
+++ comps/task1/task1.c
@@ -39,45 +39,59 @@ extern struct list_node* task1(FILE* _fff_dta){
     for(unsigned int i = 1; i <= num_tem; i++)
     {
         struct team* tem = (struct team*)malloc(sizeof(struct team));
-        if(!tem) fprintf(stderr, "Esec alocare memorie pt echipa nr %d", i);
+        {
+            if (!tem) fprintf(stderr, "Esec alocare memorie pt echipa nr %d\n", i);
+            else fprintf(stderr, "S-a alocat mem pentru echipa numarul %d\n", i);
+        }
 
-        //numarul de membri
-        unsigned int num_mem;
         //citim numarul de membri
         //fscanf(_fff_dta, "%d", &num_mem);
         char* _num_mem = read(_fff_dta, 0, " ");
-        sscanf(_num_mem, "%d", &num_mem);
-        free(_num_mem);
+        {
+            if (!num_tem) fprintf(stderr, "Nu s-a putut citi numarul de membrii ai echipei\n");
+            else {
+                sscanf(_num_mem, "%d", &tem->number);
+                fprintf(stderr, "S-a citi numerul de membrii ai echipei : %d\n", tem->number);
+                free(_num_mem);
+            }
+        }
+
 
         //initializam vectorul de coechipieri
-        struct player* tem_mem = (struct player*)malloc(sizeof(struct player) * num_mem);
-        //eroare daca nu s-a putut aloca memoria
-        if(!tem_mem) fprintf(stderr, "Nu s-a putut aloca memorie pentru membri pentru echipa nr %d\n", i);
-        else fprintf(stderr, "S-a alocat mem pentru membrii echipei %d, nr mem %d\n", i, num_mem);
+        tem->members = (struct player*)malloc(sizeof(struct player) * tem->number);
+        {
+            if (!tem->members) fprintf(stderr, "Nu s-a putut aloca memorie pentru membri pentru echipa nr %d\n", i);
+            else fprintf(stderr, "S-a alocat mem pentru membrii echipei %d\n", i);
+        }
 
         //citire si alocare nume echipa
         tem->name = read(_fff_dta, 0, "\n");
-        //eroare daca nu s-a putut aloca memoria
-        if(!tem->name) fprintf(stderr, "Nu s-a putut aloca memorie pentru nume pentru echipa nr %d\n", i);
-        else fprintf(stderr, "s-a alocat mem pt nume : %s\n", tem->name);
+        {
+            if (!tem->name) fprintf(stderr, "Nu s-a putut aloca memorie pentru nume pentru echipa nr %d\n", i);
+            else fprintf(stderr, "s-a alocat mem pt nume : %s\n", tem->name);
+        }
 
         //pentru fiecare membru al echipei
-        for(int j = 0; j < num_mem; j = j + 1)
+        for(int j = 0; j < tem->number; j = j + 1)
         {
             //selectam membrul echipei
-            struct player* mem = tem_mem + j;
+            struct player* mem = tem->members + j;
 
             mem->last_name = read(_fff_dta, 0, " \n");
-            if(!mem->last_name) fprintf(stderr, "   Nu s-a putut aloca mem pt LN al membrului %d\n", j);
-            else fprintf(stderr, "  s-a alocat mem pt LN al membrului %d : %s\n", j, mem->last_name);
+            {
+                if (!mem->last_name) fprintf(stderr, "   Nu s-a putut aloca mem pt LN al membrului %d\n", j);
+                else fprintf(stderr, "  s-a alocat mem pt LN al membrului %d : %s\n", j, mem->last_name);
+            }
 
             mem->first_name = read(_fff_dta, 0, " \n");
-            if(!mem->last_name) fprintf(stderr, "   Nu s-a putut aloca mem pt FN al membrului %d\n", j);
-            else fprintf(stderr, "  s-a alocat mem pt FN al membrului %d : %s\n", j, mem->first_name);
+            {
+                if (!mem->last_name) fprintf(stderr, "   Nu s-a putut aloca mem pt FN al membrului %d\n", j);
+                else fprintf(stderr, "  s-a alocat mem pt FN al membrului %d : %s\n", j, mem->first_name);
+            }
 
             char* tmp_mem_points = read(_fff_dta, 0, " \n");
-            sscanf(tmp_mem_points, "%d", &(mem->points));
-            fprintf(stderr, "  s-a alocat mem pt nr pct al mem %d : %d\n\n", j, mem->points);
+            sscanf(tmp_mem_points, "%f", &(mem->points));
+            { fprintf(stderr, "  s-a alocat mem pt nr pct al mem %d : %f\n\n", j, mem->points); }
             free(tmp_mem_points);
         }
 
